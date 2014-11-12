c05126072f2ea672fc7ef3ce8430b2a4ef90f0b7
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-12 17:32:42 +0100

    Implemented class.

--- softwaresystems/src/ss/week1/Password.java
+++ softwaresystems/src/ss/week1/Password.java
@@ -3,19 +3,47 @@ package ss.week1;
 public class Password {
     public static final String INITIAL = "";
 
-    public Password() {
+    private String password;
 
+    public Password() {
+        this.password = INITIAL;
     }
 
     public boolean acceptable(String suggestion) {
-        return false;
+        if (suggestion == null) {
+            // Suggestion cannot be null.
+            return false;
+        }
+        if (suggestion.length() < 6) {
+            // Suggestion must have at least 6 characters.
+            return false;
+        }
+        if (suggestion.contains(" ")) {
+            // Suggestion cannot contain any spaces.
+            return false;
+        }
+
+        // Acceptable suggestion.
+        return true;
     }
 
     public boolean setWord(String oldPass, String newPass) {
-        return false;
+        if (!testWord(oldPass)) {
+            // Old password is invalid.
+            return false;
+        }
+        if (!acceptable(newPass)) {
+            // New password is not acceptable.
+            return false;
+        }
+
+        // Update the password to the new password.
+        password = newPass;
+
+        return true;
     }
 
     public boolean testWord(String word) {
-        return false;
+        return word.equals(password);
     }
 }
