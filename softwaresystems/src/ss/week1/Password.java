e6e4f2995355ed0d32fb07d7496df6a77504386e
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-12 17:26:47 +0100

    Initial commit.

---
+++ softwaresystems/src/ss/week1/Password.java
@@ -0,0 +1,21 @@
+package ss.week1;
+
+public class Password {
+    public static final String INITIAL = "";
+
+    public Password() {
+
+    }
+
+    public boolean acceptable(String suggestion) {
+        return false;
+    }
+
+    public boolean setWord(String oldPass, String newPass) {
+        return false;
+    }
+
+    public boolean testWord(String word) {
+        return false;
+    }
+}
