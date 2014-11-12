c3b545e0cd34cc7dd3ebfc0b31c1ee671e36f660
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-12 16:42:26 +0100

    Initial commit.

---
+++ softwaresystems/src/ss/week1/hotel/Guest.java
@@ -0,0 +1,23 @@
+package ss.week1.hotel;
+
+public class Guest {
+    public Guest(String argName) {
+
+    }
+
+    public boolean checkin(Room argRoom) {
+        return false;
+    }
+
+    public boolean checkout() {
+        return false;
+    }
+
+    public String getName() {
+        return null;
+    }
+
+    public Room getRoom() {
+        return null;
+    }
+}
