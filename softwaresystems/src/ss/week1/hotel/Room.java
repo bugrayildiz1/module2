f91bcd4b91a4d76db71e2bce0f22d4a8be61cea2
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-12 17:14:48 +0100

    Added toString.

--- softwaresystems/src/ss/week1/hotel/Room.java
+++ softwaresystems/src/ss/week1/hotel/Room.java
@@ -18,7 +18,7 @@ public class Room {
      * @param no number of the new <code>Room</code>
      */
     public Room(int no) {
-    	number = no;
+        number = no;
     }
 
     // ------------------ Queries --------------------------
@@ -27,28 +27,31 @@ public class Room {
      * Returns the number of this <code>Room</code>.
      */
     public int getNumber() {
-    	return number;
+        return number;
     }
 
     /**
      * Returns the current guest living in this <code>Room</code>.
-     * @return the guest of this <code>Room</code>;
-     *         <code>null</code> if this <code>Room</code> 
-     *         is not currently rented
+     * @return the guest of this <code>Room</code>; <code>null</code> if this <code>Room</code> is
+     * not currently rented
      */
     public Guest getGuest() {
-    	return guest;
+        return guest;
     }
 
     // ------------------ Commands --------------------------
 
     /**
      * Assigns a <code>Guest</code> to this <code>Room</code>.
-     * @param g the next guest renting this <code>Room</code>;
-     *        if <code>null</code> is passed, this <code>Room</code>
-     *        is empty afterwards
+     * @param g the next guest renting this <code>Room</code>; if <code>null</code> is passed, this
+     * <code>Room</code> is empty afterwards
      */
     public void setGuest(Guest g) {
-    	guest = g;
+        guest = g;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Room %d", number);
     }
 }
