530c5ca06392f2c3eac545c9ae290d730254acf0
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-18 02:10:03 +0100

    Changed to actually make use of the Checker interface.

--- softwaresystems/src/ss/week3/pw/Password.java
+++ softwaresystems/src/ss/week3/pw/Password.java
@@ -1,8 +1,6 @@
 package ss.week3.pw;
 
 public class Password {
-    public static final String INITIAL = "";
-
     private final Checker checker;
     private final String initPass;
     private String password;
@@ -18,34 +16,14 @@ public class Password {
     }
 
     public boolean acceptable(String suggestion) {
-        if (suggestion == null) {
-            // Suggestion cannot be null.
-            return false;
-        }
-        if (suggestion.length() < 6) {
-            // Suggestion must have at least 6 characters.
-            return false;
-        }
-        if (suggestion.contains(" ")) {
-            // Suggestion cannot contain any spaces.
-            return false;
-        }
-
-        // Acceptable suggestion.
-        return true;
+        return checker.acceptable(suggestion);
     }
 
     public boolean setWord(String oldPass, String newPass) {
-        if (!testWord(oldPass)) {
-            // Old password is invalid.
-            return false;
-        }
-        if (!acceptable(newPass)) {
-            // New password is not acceptable.
+        if (!testWord(oldPass) || !acceptable(newPass)) {
             return false;
         }
 
-        // Update the password to the new password.
         password = newPass;
 
         return true;
