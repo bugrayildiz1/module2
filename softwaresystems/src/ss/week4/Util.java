d088d506e7721ca37a360f91963337345eb2e1ff
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-18 03:37:16 +0100

    P-4.3.

--- softwaresystems/src/ss/week4/Util.java
+++ softwaresystems/src/ss/week4/Util.java
@@ -1,31 +1,31 @@
 package ss.week4;
 
 import java.util.ArrayList;
-import java.lang.String;
 import java.util.List;
 
 public class Util {
-    
-    public static <Element> List<Element> zip
-    (List<Element> l1, List<Element> l2) {
-        ArrayList<Element> RESULT = new ArrayList<Element>();
-        for (int i = 0; i<l1.size(); i++) {
-            RESULT.add(l1.get(i));
-            RESULT.add(l2.get(i));
+
+    public static <E> List<E> zip(List<E> l1, List<E> l2) {
+        ArrayList<E> result = new ArrayList<E>();
+        for (int i = 0; i < l1.size(); i++) {
+            result.add(l1.get(i));
+            result.add(l2.get(i));
         }
-        return RESULT;
-    }    
+        return result;
+    }
 
     /**
-     * signum function
+     * signum function.
      * @param i the function argument
      * @return -1, 0 or 1 if the argument is negative, 0 or positive
      */
-    public static int signum(int i) {
-        if(i < 0)
+    public static int sigNum(int i) {
+        if (i < 0) {
             return -1;
-        else if (i > 0)
+        } else if (i > 0) {
             return 1;
-        else return 0;
+        } else {
+            return 0;
+        }
     }
 }
