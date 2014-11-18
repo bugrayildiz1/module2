f9849407358ef910cf269496440df36f7f7ea3b8
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-18 03:09:14 +0100

    Fixed Checkstyle warnings.

--- softwaresystems/src/ss/week3/hotel/Fridge.java
+++ softwaresystems/src/ss/week3/hotel/Fridge.java
@@ -8,39 +8,39 @@ import ss.week3.hotel.Bill.Item;
  * @author Maarten Everts
  */
 public class Fridge implements Item {
-	final static int PRICE_PER_DRINK = 2;
-	private int numberOfDrinks = 0;
-	
-	/**
-	 * Construct a Fridge with a certain number of drinks used.
-	 * @param numberOfDrinks number of drinks used from the fridge.
-	 */
-	public Fridge(int numberOfDrinks) {
-		this.numberOfDrinks = numberOfDrinks;
-	}
-	
-	/**
-	 * Set the number of drinks used from the fridge.
-	 * @param numberOfDrinks
-	 */
-	public void setNumberOfDrinks(int numberOfDrinks) {
-		this.numberOfDrinks = numberOfDrinks;
-	}
-	
-	/**
-	 * Return the current number of drinks used from the fridge.
-	 * @return current number of drinks used from the fridge.
-	 */
-	public int getNumberOfDrinks() {
-		return numberOfDrinks;
-	}
-	
-	/**
-	 * Returns the total cost of the items used from the fridge 
-	 */
-	@Override
-	public double getAmount() {
-		return (double)(numberOfDrinks*PRICE_PER_DRINK);
-	}
+    final static int PRICE_PER_DRINK = 2;
+    private int numberOfDrinks = 0;
+
+    /**
+     * Construct a Fridge with a certain number of drinks used.
+     * @param argNumberOfDrinks number of drinks used from the fridge.
+     */
+    public Fridge(int argNumberOfDrinks) {
+        this.numberOfDrinks = argNumberOfDrinks;
+    }
+
+    /**
+     * Set the number of drinks used from the fridge.
+     * @param argNumberOfDrinks
+     */
+    public void setNumberOfDrinks(int argNumberOfDrinks) {
+        this.numberOfDrinks = argNumberOfDrinks;
+    }
+
+    /**
+     * Return the current number of drinks used from the fridge.
+     * @return current number of drinks used from the fridge.
+     */
+    public int getNumberOfDrinks() {
+        return numberOfDrinks;
+    }
+
+    /**
+     * Returns the total cost of the items used from the fridge.
+     */
+    @Override
+    public double getAmount() {
+        return (double) (numberOfDrinks * PRICE_PER_DRINK);
+    }
 
 }
