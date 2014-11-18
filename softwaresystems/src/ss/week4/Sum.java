ff9357aa7114661a2ebc0dcd1d78b7c963752543
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-18 04:01:37 +0100

    P-4.6 Initial commit.

---
+++ softwaresystems/src/ss/week4/Sum.java
@@ -0,0 +1,26 @@
+package ss.week4;
+
+public class Sum implements Function {
+    private final Function g;
+    private final Function h;
+
+    public Sum(Function argG, Function argH) {
+        this.g = argG;
+        this.h = argH;
+    }
+
+    @Override
+    public double apply(double x) {
+        return g.apply(x) + h.apply(x);
+    }
+
+    @Override
+    public Function derivative() {
+        return new Sum(g.derivative(), h.derivative());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s + %s", g.toString(), h.toString());
+    }
+}
