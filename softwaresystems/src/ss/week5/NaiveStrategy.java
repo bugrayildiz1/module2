06c6de04f4659f198e1a39d88d8601d9972ff06c
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-19 02:46:10 +0100

    P-5.11 Initial commit.

---
+++ softwaresystems/src/ss/week5/NaiveStrategy.java
@@ -0,0 +1,27 @@
+package ss.week5;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NaiveStrategy implements Strategy {
+
+    @Override
+    public String getName() {
+        return "Naive";
+    }
+
+    @Override
+    public int determineMove(Board b, Mark m) {
+        List<Integer> freeSlots = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < Board.DIM * Board.DIM; i++) {
+            if (b.isEmptyField(i)) {
+                freeSlots.add(i);
+            }
+        }
+
+        return freeSlots.get(random.nextInt(freeSlots.size()));
+    }
+}
