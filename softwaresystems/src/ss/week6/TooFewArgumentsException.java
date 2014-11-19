969029efa075477a83992c83cdd6e8c4d26e833e
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-19 16:00:10 +0100

    P-6.3 Initial commit.

---
+++ softwaresystems/src/ss/week6/TooFewArgumentsException.java
@@ -0,0 +1,6 @@
+package ss.week6;
+
+public class TooFewArgumentsException extends WrongArgumentException {
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ softwaresystems/src/ss/week6/WrongArgumentException.java
@@ -0,0 +1,6 @@
+package ss.week6;
+
+public class WrongArgumentException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+}
