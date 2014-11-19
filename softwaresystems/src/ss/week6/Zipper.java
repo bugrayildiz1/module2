4e47aa6ed75de7789efca328a7c9d97afa6da819
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-19 16:07:34 +0100

    P-6.3.

--- softwaresystems/src/ss/week6/Zipper.java
+++ softwaresystems/src/ss/week6/Zipper.java
@@ -12,18 +12,35 @@ public class Zipper {
         }
         return result;
     }
-    
+
+    /*@
+    requires s1 != null & s2 != null;
+    requires s1.length() == s2.length();
+    */
+    public static String zip2(String s1, String s2) throws TooFewArgumentsException,
+            ArgumentLengthsDifferException {
+        if (s1 == null || s2 == null) {
+            throw new TooFewArgumentsException("must pass two command line arguments");
+        } else if (s1.length() != s2.length()) {
+            throw new ArgumentLengthsDifferException("length of command line arguments differs ("
+                    + s1.length() + ", " + s2.length() + ")");
+        }
+
+        String result = "";
+        for (int i = 0; i < s1.length(); i++) {
+            result += Character.toString(s1.charAt(i)) + Character.toString(s2.charAt(i));
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         String s1 = args.length >= 1 ? args[0] : null;
         String s2 = args.length >= 2 ? args[1] : null;
-        if (s1 == null || s2 == null) {
-            System.out.println("error: must pass two command line arguments");
-        } else if (s1.length() != s2.length()) {
-            System.out.println("error: length of command line arguments differs ("
-                    + s1.length() + ", " + s2.length() + ")");
-        } else {
-            System.out.println(zip(s1, s2));
+
+        try {
+            System.out.println(zip2(s1, s2));
+        } catch (TooFewArgumentsException | ArgumentLengthsDifferException e) {
+            System.out.println("error: " + e.getMessage());
         }
     }
 }
