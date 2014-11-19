69bcc56b3a64d9b34e67fb22c39ab3a320d7799e
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-19 02:19:13 +0100

    P-5.7.

--- softwaresystems/src/ss/week5/Board.java
+++ softwaresystems/src/ss/week5/Board.java
@@ -11,8 +11,8 @@ public class Board {
     // -- Constants --------------------------------------------------
 
     public static final int DIM = 3;
-    private static final String[] NUMBERING = {" 0 | 1 | 2 ", "---+---+---",
-        " 3 | 4 | 5 ", "---+---+---", " 6 | 7 | 8 "};
+    private static final String[] NUMBERING = {" 0 | 1 | 2 ", "---+---+---", " 3 | 4 | 5 ",
+        "---+---+---", " 6 | 7 | 8 "};
     private static final String LINE = NUMBERING[1];
     private static final String DELIM = "     ";
 
@@ -24,8 +24,7 @@ public class Board {
            getField(i) == Mark.EMPTY || getField(i) == Mark.XX || getField(i) == Mark.OO);
      */
     /**
-     * The DIM by DIM fields of the Tic Tac Toe student. See NUMBERING for the
-     * coding of the fields.
+     * The DIM by DIM fields of the Tic Tac Toe student. See NUMBERING for the coding of the fields.
      */
     private Mark[] fields;
 
@@ -38,7 +37,11 @@ public class Board {
      * Creates an empty student.
      */
     public Board() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
+        this.fields = new Mark[DIM * DIM];
+
+        for (int i = 0; i < fields.length; i++) {
+            fields[i] = Mark.EMPTY;
+        }
     }
 
     // -- Queries ----------------------------------------------------
@@ -51,25 +54,26 @@ public class Board {
      * Creates a deep copy of this field.
      */
     public Board deepCopy() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return null;
-    }
+        Board board = new Board();
 
+        for (int i = 0; i < fields.length; i++) {
+            board.fields[i] = fields[i];
+        }
+
+        return board;
+    }
 
     /*@
        requires 0 <= row & row < DIM;
        requires 0 <= col & col < DIM;
      */
     /**
-     * Calculates the index in the linear array of fields from a (row, col)
-     * pair.
+     * Calculates the index in the linear array of fields from a (row, col) pair.
      * @return the index belonging to the (row,col)-field
      */
     public int index(int row, int col) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return 0;
+        return row * DIM + col;
     }
-
 
     /*@
        ensures \result == (0 <= ix && ix < DIM * DIM);
@@ -80,8 +84,7 @@ public class Board {
      */
     /*@pure*/
     public boolean isField(int ix) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return ix >= 0 && ix < DIM * DIM;
     }
 
     /*@
@@ -94,10 +97,8 @@ public class Board {
      */
     /*@pure*/
     public boolean isField(int row, int col) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return row >= 0 && row < DIM && col >= 0 && col < DIM;
     }
-
 
     /*@
        requires this.isField(i);
@@ -106,13 +107,11 @@ public class Board {
     /**
      * Returns the content of the field <code>i</code>.
      * 
-     * @param i
-     *            the number of the field (see NUMBERING)
+     * @param i the number of the field (see NUMBERING)
      * @return the mark on the field
      */
     public Mark getField(int i) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return null;
+        return fields[i];
     }
 
     /*@
@@ -122,15 +121,12 @@ public class Board {
     /**
      * Returns the content of the field referred to by the (row,col) pair.
      * 
-     * @param row
-     *            the row of the field
-     * @param col
-     *            the column of the field
+     * @param row the row of the field
+     * @param col the column of the field
      * @return the mark on the field
      */
     public Mark getField(int row, int col) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return null;
+        return getField(index(row, col));
     }
 
     /*@
@@ -140,13 +136,11 @@ public class Board {
     /**
      * Returns true if the field <code>i</code> is empty.
      * 
-     * @param i
-     *            the index of the field (see NUMBERING)
+     * @param i the index of the field (see NUMBERING)
      * @return true if the field is empty
      */
     public boolean isEmptyField(int i) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return fields[i] == Mark.EMPTY;
     }
 
     /*@
@@ -157,16 +151,13 @@ public class Board {
     /**
      * Returns true if the field referred to by the (row,col) pair it empty.
      * 
-     * @param row
-     *            the row of the field
-     * @param col
-     *            the column of the field
+     * @param row the row of the field
+     * @param col the column of the field
      * @return true if the field is empty
      */
     /*@pure*/
     public boolean isEmptyField(int row, int col) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return isEmptyField(index(row, col));
     }
 
     /*@
@@ -179,8 +170,13 @@ public class Board {
      */
     /*@pure*/
     public boolean isFull() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i] != Mark.EMPTY) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /*@
@@ -188,54 +184,93 @@ public class Board {
 
      */
     /**
-     * Returns true if the game is over. The game is over when there is a winner
-     * or the whole student is full.
+     * Returns true if the game is over. The game is over when there is a winner or the whole
+     * student is full.
      * 
      * @return true if the game is over
      */
     /*@pure*/
     public boolean gameOver() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return isFull() || hasWinner();
     }
 
     /**
-     * Checks whether there is a row which is full and only contains the mark
-     * <code>m</code>.
+     * Checks whether there is a row which is full and only contains the mark <code>m</code>.
      * 
-     * @param m
-     *            the mark of interest
+     * @param m the mark of interest
      * @return true if there is a row controlled by <code>m</code>
      */
     public boolean hasRow(Mark m) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
+        for (int row = 0; row < DIM; row++) {
+            boolean hasRow = true;
+
+            for (int col = 0; col < DIM; col++) {
+                if (getField(row, col) != m) {
+                    hasRow = false;
+                    break;
+                }
+            }
+
+            if (hasRow) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     /**
-     * Checks whether there is a column which is full and only contains the mark
-     * <code>m</code>.
+     * Checks whether there is a column which is full and only contains the mark <code>m</code>.
      * 
-     * @param m
-     *            the mark of interest
+     * @param m the mark of interest
      * @return true if there is a column controlled by <code>m</code>
      */
     public boolean hasColumn(Mark m) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
+        for (int col = 0; col < DIM; col++) {
+            boolean hasCol = true;
+
+            for (int row = 0; row < DIM; row++) {
+                if (getField(row, col) != m) {
+                    hasCol = false;
+                    break;
+                }
+            }
+
+            if (hasCol) {
+                return true;
+            }
+        }
+
         return false;
     }
 
     /**
-     * Checks whether there is a diagonal which is full and only contains the
-     * mark <code>m</code>.
+     * Checks whether there is a diagonal which is full and only contains the mark <code>m</code>.
      * 
-     * @param m
-     *            the mark of interest
+     * @param m the mark of interest
      * @return true if there is a diagonal controlled by <code>m</code>
      */
     public boolean hasDiagonal(Mark m) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        boolean hasLeftToRight = true;
+        boolean hasRightToLeft = true;
+
+        for (int i = 0; i < DIM; i++) {
+            if (getField(i, i) != m) {
+                hasLeftToRight = false;
+                break;
+            }
+        }
+
+        if (!hasLeftToRight) {
+            for (int i = 0; i < DIM; i++) {
+                if (getField(i, DIM - i - 1) != m) {
+                    hasRightToLeft = false;
+                    break;
+                }
+            }
+        }
+
+        return hasLeftToRight || hasRightToLeft;
     }
 
     /*@
@@ -245,17 +280,15 @@ public class Board {
                                 this.hasDiagonal(m);
      */
     /**
-     * Checks if the mark <code>m</code> has won. A mark wins if it controls at
-     * least one row, column or diagonal.
+     * Checks if the mark <code>m</code> has won. A mark wins if it controls at least one row,
+     * column or diagonal.
      * 
-     * @param m
-     *            the mark of interest
+     * @param m the mark of interest
      * @return true if the mark has won
      */
     /*@pure*/
     public boolean isWinner(Mark m) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return hasRow(m) || hasColumn(m) || hasDiagonal(m);
     }
 
     /*@
@@ -263,20 +296,19 @@ public class Board {
 
      */
     /**
-     * Returns true if the game has a winner. This is the case when one of the
-     * marks controls at least one row, column or diagonal.
+     * Returns true if the game has a winner. This is the case when one of the marks controls at
+     * least one row, column or diagonal.
      * 
      * @return true if the student has a winner.
      */
     /*@pure*/
     public boolean hasWinner() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-        return false;
+        return isWinner(Mark.XX) || isWinner(Mark.OO);
     }
 
     /**
-     * Returns a String representation of this student. In addition to the current
-     * situation, the String also shows the numbering of the fields.
+     * Returns a String representation of this student. In addition to the current situation, the
+     * String also shows the numbering of the fields.
      * 
      * @return the game situation as String
      */
@@ -304,11 +336,12 @@ public class Board {
        ensures (\forall int i; 0 <= i & i < DIM * DIM; this.getField(i) == Mark.EMPTY);
      */
     /**
-     * Empties all fields of this student (i.e., let them refer to the value
-     * Mark.EMPTY).
+     * Empties all fields of this student (i.e., let them refer to the value Mark.EMPTY).
      */
     public void reset() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
+        for (int i = 0; i < fields.length; i++) {
+            fields[i] = Mark.EMPTY;
+        }
     }
 
     /*@
@@ -318,13 +351,11 @@ public class Board {
     /**
      * Sets the content of field <code>i</code> to the mark <code>m</code>.
      * 
-     * @param i
-     *            the field number (see NUMBERING)
-     * @param m
-     *            the mark to be placed
+     * @param i the field number (see NUMBERING)
+     * @param m the mark to be placed
      */
     public void setField(int i, Mark m) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
+        fields[i] = m;
     }
 
     /*@
@@ -333,18 +364,14 @@ public class Board {
 
      */
     /**
-     * Sets the content of the field represented by the (row,col) pair to the
-     * mark <code>m</code>.
+     * Sets the content of the field represented by the (row,col) pair to the mark <code>m</code>.
      * 
-     * @param row
-     *            the field's row
-     * @param col
-     *            the field's column
-     * @param m
-     *            the mark to be placed
+     * @param row the field's row
+     * @param col the field's column
+     * @param m the mark to be placed
      */
     public void setField(int row, int col, Mark m) {
-        // [BODY-NOG-TOE-TE-VOEGEN]
+        setField(index(row, col), m);
     }
 
 }
