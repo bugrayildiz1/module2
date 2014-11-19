d3a37b9c5fcb3eeab3cda69c7d87c792e2eba61d
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-19 02:26:57 +0100

    P-5.8.

--- softwaresystems/src/ss/week5/Game.java
+++ softwaresystems/src/ss/week5/Game.java
@@ -48,10 +48,8 @@ public class Game {
     /**
      * Creates a new Game object.
      * 
-     * @param s0
-     *            the first player
-     * @param s1
-     *            the second player
+     * @param s0 the first player
+     * @param s1 the second player
      */
     public Game(Player s0, Player s1) {
         board = new Board();
@@ -65,8 +63,8 @@ public class Game {
 
     /**
      * Starts the Tic Tac Toe game. <br>
-     * Asks after each ended game if the user want to continue. Continues until
-     * the user does not want to play anymore.
+     * Asks after each ended game if the user want to continue. Continues until the user does not
+     * want to play anymore.
      */
     public void start() {
         boolean doorgaan = true;
@@ -78,17 +76,14 @@ public class Game {
     }
 
     /**
-     * Prints a question which can be answered by yes (true) or no (false).
-     * After prompting the question on standard out, this method reads a String
-     * from standard in and compares it to the parameters for yes and no. If the
-     * user inputs a different value, the prompt is repeated and te method reads
-     * input again.
+     * Prints a question which can be answered by yes (true) or no (false). After prompting the
+     * question on standard out, this method reads a String from standard in and compares it to the
+     * parameters for yes and no. If the user inputs a different value, the prompt is repeated and
+     * te method reads input again.
      * 
      * @parom prompt the question to print
-     * @param yes
-     *            the String corresponding to a yes answer
-     * @param no
-     *            the String corresponding to a no answer
+     * @param yes the String corresponding to a yes answer
+     * @param no the String corresponding to a no answer
      * @return true is the yes answer is typed, false if the no answer is typed
      */
     private boolean readBoolean(String prompt, String yes, String no) {
@@ -112,21 +107,33 @@ public class Game {
 
     /**
      * Plays the Tic Tac Toe game. <br>
-     * First the (still empty) board is shown. Then the game is played until it
-     * is over. Players can make a move one after the other. After each move,
-     * the changed game situation is printed.
+     * First the (still empty) board is shown. Then the game is played until it is over. Players can
+     * make a move one after the other. After each move, the changed game situation is printed.
      */
     private void play() {
-        // [BODY-NOG-TOE-TE-VOEGEN]
-    }
+        // Print initial empty board.
+        update();
 
+        while (!board.gameOver()) {
+            Player currentPlayer = players[current];
+
+            // Let the current player make his move, then print the updated board.
+            currentPlayer.makeMove(board);
+            update();
+
+            // Swap to the next player.
+            current = current + 1 % players.length;
+        }
+
+        // Game is over, print result.
+        printResult();
+    }
 
     /**
      * Prints the game situation.
      */
     private void update() {
-        System.out.println("\ncurrent game situation: \n\n" + board.toString()
-                + "\n");
+        System.out.println("\ncurrent game situation: \n\n" + board.toString() + "\n");
     }
 
     /*@
@@ -138,10 +145,9 @@ public class Game {
      */
     private void printResult() {
         if (board.hasWinner()) {
-            Player winner = board.isWinner(players[0].getMark()) ? players[0]
-                    : players[1];
-            System.out.println("Speler " + winner.getName() + " ("
-                    + winner.getMark().toString() + ") has won!");
+            Player winner = board.isWinner(players[0].getMark()) ? players[0] : players[1];
+            System.out.println("Speler " + winner.getName() + " (" + winner.getMark().toString()
+                    + ") has won!");
         } else {
             System.out.println("Draw. There is no winner!");
         }
