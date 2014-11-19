0da0788c988065085c58e48e09b5ab17e9838a86
gixslayer <ciske_harsema_2@hotmail.com>
2014-11-19 02:56:03 +0100

    P-5.13.

--- softwaresystems/src/ss/week5/TicTacToe.java
+++ softwaresystems/src/ss/week5/TicTacToe.java
@@ -5,11 +5,19 @@ public class TicTacToe {
     public static void main(String[] args) {
         assert args.length >= 2;
 
-        Player playerA = new HumanPlayer(args[0], Mark.OO);
-        Player playerB = new HumanPlayer(args[1], Mark.XX);
+        Player playerA = getPlayer(args[0], Mark.XX);
+        Player playerB = getPlayer(args[1], Mark.OO);
         Game game = new Game(playerA, playerB);
 
         game.start();
+    }
+
+    private static Player getPlayer(String name, Mark mark) {
+        if (name.equals("-N")) {
+            return new ComputerPlayer(mark, new NaiveStrategy());
+        }
+
+        return new HumanPlayer(name, mark);
     }
 
 }
