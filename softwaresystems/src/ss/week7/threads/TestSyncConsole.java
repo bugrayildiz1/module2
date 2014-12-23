92cb3d755499eb734dcdc70ac899a57d05989453
gixslayer <ciske@ciske-laptop>
2014-12-23 14:09:23 +0100

    P-7.13.

--- softwaresystems/src/ss/week7/threads/TestSyncConsole.java
+++ softwaresystems/src/ss/week7/threads/TestSyncConsole.java
@@ -1,7 +1,22 @@
 package ss.week7.threads;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TestSyncConsole extends Thread {
-    private static final Object SYNC_ROOT = new Object();
+    //private static final Object SYNC_ROOT = new Object();
+    private static final Lock LOCK = new ReentrantLock();
+
+    // P-7.12:
+    // 1: A ReentrantLock is owned by the thread last successfully locking, but not yet unlocking 
+    // it. A thread invoking lock will return, successfully acquiring the lock, when the lock is 
+    // not owned by another thread. The method will return immediately if the current thread 
+    // already owns the lock.
+    // 2: They are essentially the same, but the Reentrantlock is a bit more flexible and is
+    // unstructured (unlike synchronized blocks) allowing for code that isn't possible with a
+    // single synchronized block (such as locking in one method and unlocking in another).
+    // 3: As mentioned in 2, it is unstructured.
+    // 4: More code (try-finally construct suggested) which might reduce readability.
 
     public TestSyncConsole(String name) {
         super(name);
@@ -16,13 +31,15 @@ public class TestSyncConsole extends Thread {
     // on an object to interleave, but as several objects are created and each object will only
     // invoke sum once it has no effect.
     private synchronized void sum() {
-        synchronized (SYNC_ROOT) {
-            int num1 = SyncConsole.readInt(String.format("%s: get number 1? ", getName()));
-            int num2 = SyncConsole.readInt(String.format("%s: get number 2? ", getName()));
-            int sum = num1 + num2;
+        //synchronized (SYNC_ROOT) {
+        LOCK.lock();
+        int num1 = SyncConsole.readInt(String.format("%s: get number 1? ", getName()));
+        int num2 = SyncConsole.readInt(String.format("%s: get number 2? ", getName()));
+        int sum = num1 + num2;
 
-            SyncConsole.println(String.format("%s: %d + %d = %d", getName(), num1, num2, sum));
-        }
+        SyncConsole.println(String.format("%s: %d + %d = %d", getName(), num1, num2, sum));
+        LOCK.unlock();
+        //}
     }
 
     public static void main(String[] args) {
