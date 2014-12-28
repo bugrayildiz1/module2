ad234564ecc62abde8781400523d733daad67de8
gixslayer <ciske_harsema_2@hotmail.com>
2014-12-28 15:27:50 +0100

    Initial import from lab files.

---
+++ softwaresystems/src/ss/week7/cmdline/Server.java
@@ -0,0 +1,16 @@
+package ss.week7.cmdline;
+
+
+/**
+ * Server.
+ * @author Theo Ruys
+ * @version 2005.02.21
+ */
+public class Server {
+    private static final String USAGE = "usage: " + Server.class.getName() + " <name> <port>";
+
+    /** Start een Server-applicatie op. */
+    public static void main(String[] args) {
+    }
+
+} // end of class Server
