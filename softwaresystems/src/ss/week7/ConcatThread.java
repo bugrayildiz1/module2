7b7ab510b008b947e3649e891e11f646b8453ae1
gixslayer <ciske_harsema_2@hotmail.com>
2014-12-28 14:44:37 +0100

    P-7.19.3.

--- softwaresystems/src/ss/week7/ConcatThread.java
+++ softwaresystems/src/ss/week7/ConcatThread.java
@@ -9,11 +9,26 @@ public class ConcatThread extends Thread {
     }
 
     public void run() {
-        text = text.concat(toe);
+        // P-7.19.3: Force sequential executing of the thread code by locking on a static variable.
+        synchronized (text) {
+            text = text.concat(toe);
+        }
     }
 
-    public static void main(String[] args) {
-        (new ConcatThread("one;")).start();
-        (new ConcatThread("two;")).start();
+    public static void main(String[] args) throws InterruptedException {
+        Thread a = new ConcatThread("one;");
+        Thread b = new ConcatThread("two;");
+
+        a.start();
+        b.start();
+
+        a.join();
+        b.join();
+
+        // P-7.19.2: Possible outcomes are 'one;two;' or 'two;one;' depending on which thread is
+        // executed first and assuming they are executed sequentially. If they execute interleaved
+        // it is possible the 'result' of one thread (assigning to text) is overruled by the other
+        // thread in which case the outcome can be either 'one;' or 'two;'.
+        System.out.println(text);
     }
 }
